8942fbf7976b73369a259d31fc9b258ea2a6d811
Israel Trejo <isrtrejo07@gmail.com>
2026-01-16 18:18:08 -0600

    Added icons/pictures to the project

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -20,24 +20,32 @@ export default function SideBar() {
         <span style={{marginLeft: '12px', fontSize: '18px'}}>Social</span>
       </div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '8px'}}>
-        <div style={{...boxSize, backgroundColor: "#2a05ff"}} className="box"></div>
-        <span style={{marginLeft: '12px', fontSize: '18px'}}>Computer Science</span>
+        <div style={{...boxSize, backgroundColor: "#4444a5"}} className="box"></div>
+        <span style={{marginLeft: '12px', fontSize: '18px'}}>University</span>
       </div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '8px'}}>
-        <div style={{...boxSize, backgroundColor: "#9f05ff"}} className="box"></div>
-        <span style={{marginLeft: '12px', fontSize: '18px'}}>University</span>
+        <div style={{...boxSize, backgroundColor: "#2a05ff"}} className="box"></div>
+        <span style={{marginLeft: '12px', fontSize: '18px'}}>Computer Science</span>
       </div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '8px'}}>
         <div style={{...boxSize, backgroundColor: "#fc0000"}} className="box"></div>
         <span style={{marginLeft: '12px', fontSize: '18px'}}>Art</span>
       </div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '8px'}}>
-        <div style={{...boxSize, backgroundColor: "#14f200"}} className="box"></div>
+        <div style={{...boxSize, backgroundColor: "#14db02"}} className="box"></div>
         <span style={{marginLeft: '12px', fontSize: '18px'}}>Exercise</span>
       </div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '8px'}}>
         <div style={{...boxSize, backgroundColor: "#a97d3b"}} className="box"></div>
         <span style={{marginLeft: '12px', fontSize: '18px'}}>Organization</span>
+      </div>
+
+      <div style={{border: '2px', borderStyle: 'solid', borderColor: '#777777', padding: '10px', textAlign: 'center', cursor: 'pointer', marginTop: '12px', width: '170px'}}>
+        + Add Subject
+      </div>
+      <div style={{border: '2px', borderStyle: 'solid', borderColor: '#777777', padding: '10px', textAlign: 'center', cursor: 'pointer', marginTop: '12px', width: '170px' }}>
+        <img src="/graph.png" style={{width: "30px"}}/>
+        <p>View Analytics</p>
       </div>
     </div>
   )
